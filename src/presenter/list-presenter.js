1dc759c9d6f7f34e9b1b079a30df6b8bc53df620
Egor Osipchuk <kaban.kabanytch@yandex.ru>
2026-02-09 20:35:07 +0700

    feat: реализовал list-presenter, отделил логику от main-presenter

---
+++ src/presenter/list-presenter.js
@@ -0,0 +1,122 @@
+import { render, replace } from '../framework/render.js';
+import { updateItem } from '../utils/utils.js';
+import { SortType } from '../constants/sort-const.js';
+import { SortFns } from '../constants/sort-const.js';
+import SortView from '../view/sort-view/sort-view.js';
+import EventListView from '../view/event-list-view/event-list-view.js';
+import EmptyList from '../view/list-empty-view/list-empty-view.js';
+import EventPresenter from './event-presenter.js';
+
+export default class ListPresenter {
+  #container = null;
+  #sortComponent = null;
+
+  #events = [];
+  #sourcedEvents = [];
+
+  #eventPresenters = new Map();
+  #eventListComponent = new EventListView();
+  #emptyListComponent = new EmptyList();
+  #currentSortType = SortType.DAY;
+
+  constructor({ container }) {
+    this.#container = container;
+  }
+
+  init(events) {
+    this.#events = [...events];
+    this.#sourcedEvents = [...this.#events];
+    this.#events.sort(SortFns[this.#currentSortType]);
+    this.#render();
+  }
+
+  #renderContent() {
+    this.#renderSort();
+    this.#renderEventListComponent();
+    this.#renderEvents();
+  }
+
+  #renderSort() {
+    this.#sortComponent = new SortView({
+      onSortTypeChange: this.#handleSortTypeChange,
+      activeType: this.#currentSortType
+    });
+    render(this.#sortComponent, this.#container);
+  }
+
+  #replaceSort() {
+    if (this.#sortComponent) {
+      const newSortComponent = new SortView({
+        onSortTypeChange: this.#handleSortTypeChange,
+        activeType: this.#currentSortType
+      });
+      replace(newSortComponent, this.#sortComponent);
+      this.#sortComponent = newSortComponent;
+    }
+  }
+
+  #renderEventListComponent() {
+    render(this.#eventListComponent, this.#container);
+  }
+
+  #renderEvent(event) {
+    const eventPresenter = new EventPresenter({
+      eventListComponent: this.#eventListComponent.element,
+      onDataChange: this.#handleEventChange,
+      onModeViewChange: this.#handleModeViewChange
+    });
+    eventPresenter.init(event);
+    this.#eventPresenters.set(event.id, eventPresenter);
+  }
+
+  #renderEvents() {
+    this.#events.forEach((event) => this.#renderEvent(event));
+  }
+
+  #renderEmptyList() {
+    render(this.#emptyListComponent, this.#container);
+  }
+
+  #render() {
+    if (this.#events.length === 0) {
+      this.#renderEmptyList();
+    } else {
+      this.#renderContent();
+    }
+  }
+
+  #clearEventList() {
+    this.#eventPresenters.forEach((presenter) => presenter.destroy());
+    this.#eventPresenters.clear();
+  }
+
+
+  #sortTasks(sortType) {
+    this.#events = [...this.#sourcedEvents].sort(SortFns[sortType]);
+  }
+
+  setSortType(sortType) {
+    if (this.#currentSortType === sortType) {
+      return;
+    }
+
+    this.#currentSortType = sortType;
+    this.#sortTasks(sortType);
+    this.#replaceSort();
+    this.#clearEventList();
+    this.#renderEvents();
+  }
+
+  #handleSortTypeChange = (sortType) => {
+    this.setSortType(sortType);
+  };
+
+  #handleEventChange = (updateEvent) => {
+    this.#events = updateItem(this.#events, updateEvent);
+    this.#eventPresenters.get(updateEvent.id).init(updateEvent);
+  };
+
+  #handleModeViewChange = () => {
+    this.#eventPresenters.forEach((presenter) => presenter.resetView());
+  };
+}
--- src/presenter/main-presenter.js
+++ src/presenter/main-presenter.js
@@ -1,22 +1,9 @@
-import { render, replace } from '../framework/render.js';
-import { updateItem } from '../utils/utils.js';
-import { SortType } from '../constants/sort-const.js';
-import { SortFns } from '../constants/sort-const.js';
-import SortView from '../view/sort-view/sort-view.js';
-import EventListView from '../view/event-list-view/event-list-view.js';
-import EmptyList from '../view/list-empty-view/list-empty-view.js';
-import EventPresenter from './event-presenter.js';
+import ListPresenter from './list-presenter.js';
 
 export default class MainPresenter {
   #model = null;
   #eventsContainer = null;
-  #sortComponent = null;
-  #events = [];
-
-  #emptyListComponent = new EmptyList();
-  #eventListComponent = new EventListView();
-  #currentSortType = SortType.DAY;
-  #eventPresenters = new Map();
+  #listPresenter = null;
 
   constructor({ model, eventsContainer }) {
     this.#model = model;
@@ -24,94 +11,13 @@ export default class MainPresenter {
   }
 
   init() {
-    this.#events = [...this.#model.events
-      .map((event) => this.#model.getEventDetails(event))];
-    this.#events.sort(SortFns[this.#currentSortType]);
-    this.#renderList();
-  }
-
-  #handleModeViewChange = () => {
-    this.#eventPresenters.forEach((presenter) => presenter.resetView());
-  };
-
-  #renderEvent(event) {
-    const eventPresenter = new EventPresenter({
-      eventListComponent: this.#eventListComponent.element,
-      onDataChange: this.#handleEventChange,
-      onModeViewChange: this.#handleModeViewChange
+    const events = this.#model.events.map((event) =>
+      this.#model.getEventDetails(event)
+    );
+    this.#listPresenter = new ListPresenter({
+      container: this.#eventsContainer,
     });
-    eventPresenter.init(event);
-    this.#eventPresenters.set(event.id, eventPresenter);
+    this.#listPresenter.init(events);
   }
 
-  #renderEmptyList() {
-    render(this.#emptyListComponent, this.#eventsContainer);
-  }
-
-  #renderEvents() {
-    this.#events.forEach((event) => this.#renderEvent(event));
-  }
-
-  #renderList() {
-    if (this.#model.isEmpty()) {
-      this.#renderEmptyList();
-    } else {
-      this.#renderContent();
-    }
-  }
-
-  #clearList() {
-    this.#eventPresenters.forEach((presenter) => presenter.destroy());
-    this.#eventPresenters.clear();
-  }
-
-  #renderSort() {
-    this.#sortComponent = new SortView({
-      onSortTypeChange: this.#handleSortTypeChange,
-      activeType: this.#currentSortType
-    });
-    render(this.#sortComponent, this.#eventsContainer);
-  }
-
-  #replaceSort() {
-    if (this.#sortComponent) {
-      const newSortComponent = new SortView({
-        onSortTypeChange: this.#handleSortTypeChange,
-        activeType: this.#currentSortType
-      });
-      replace(newSortComponent, this.#sortComponent);
-      this.#sortComponent = newSortComponent;
-    }
-  }
-
-  #renderListComponent() {
-    render(this.#eventListComponent, this.#eventsContainer);
-  }
-
-  #renderContent() {
-    this.#renderSort();
-    this.#renderListComponent();
-    this.#renderEvents();
-  }
-
-  #handleEventChange = (updateEvent) => {
-    this.#events = updateItem(this.#events, updateEvent);
-    this.#eventPresenters.get(updateEvent.id).init(updateEvent);
-  };
-
-  #handleSortTypeChange = (sortType) => {
-    if (this.#currentSortType === sortType) {
-      return;
-    }
-    this.#currentSortType = sortType;
-
-    this.#sortTasks(sortType);
-    this.#clearList();
-    this.#replaceSort();
-    this.#renderEvents();
-  };
-
-  #sortTasks(sortType) {
-    this.#events = [...this.#events].sort(SortFns[sortType]);
-  }
 }
