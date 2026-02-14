8fc0b3a3d6780990d747f6ea9268ff280e8c76be
Egor Osipchuk <kaban.kabanytch@yandex.ru>
2026-02-14 17:27:23 +0700

    fix: отредактировал функцию getDuration через плагин duration

--- src/constants/const.js
+++ src/constants/const.js
@@ -1,6 +1,7 @@
 export const DateFormat = {
   DATE_FROM_FORMAT: 'MMM D',
   FULL_DATE_FORMAT: 'DD/MM/YY HH:mm',
+  DURATION_FORMAT: 'D[d] H[h] m[m]',
 };
 export const ESC_KEY = 'Escape';
 export const Mode = {
